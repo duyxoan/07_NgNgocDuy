86ab3fc16d49a3346e915516284bf57fb0f1992a
duyxoan <ngocduy060302@gmail.com>
2023-08-02 21:15:48 +0700

    update

--- SQL/Assignment 1/extra_assignment_1.sql
+++ SQL/Assignment 1/extra_assignment_1.sql
@@ -4,14 +4,14 @@ USE extra_system_assignment_1;
 DROP TABLE IF EXISTS trainee;
 CREATE TABLE trainee (
     traineeid INT AUTO_INCREMENT PRIMARY KEY,
-    fullname NVARCHAR(50),
-    birth_day DATE,
-    gender ENUM('male', 'female', 'unknown'),
-    ET_IQ INT check (ET_IQ <=20),
-    ET_Gmath INT check (ET_Gmath <=20),
-    ET_English INT check (ET_English <=50),
-    Tranining_class VARCHAR(50),
-    evaluation_notes text
+    fullname NVARCHAR(50) NOT NULL,
+    birth_day DATE NOT NULL,
+    gender ENUM('male', 'female', 'unknown') NOT NULL,
+    ET_IQ INT unsigned CHECK (ET_IQ <= 20) NOT NULL,
+    ET_Gmath INT unsigned CHECK (ET_Gmath <= 20) NOT NULL,
+    ET_English INT unsigned CHECK (ET_English <= 50) NOT NULL,
+    Tranining_class CHAR(6) NOT NULL,
+    evaluation_notes TEXT
 );
 ALTER TABLE trainee 
 ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;          
@@ -19,9 +19,9 @@ ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;
 DROP TABLE IF EXISTS datatypes ;
 CREATE TABLE datatypes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    namee VARCHAR(50),
-    datacode CHAR(5),
-    modifiedday DATE
+    namee VARCHAR(50) NOT NULL,
+    datacode CHAR(5) NOT NULL,
+    modifiedday DATETIME DEFAULT NOW()
 );
 -- exercise3--
 DROP TABLE IF EXISTS datatypes2 ;
@@ -29,7 +29,7 @@ CREATE TABLE datatypes2 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     namee VARCHAR(50),
     bỉrthday DATE,
-    gender BIT,
-    isdeletedflag BOOL
+    gender ENUM('0', '1', '2'),
+    isdeletedflag ENUM('0', '1')
 )
             
