edd2dbd09eb520fcd9e25066ee3d50ded30e3893
duyxoan <ngocduy060302@gmail.com>
2023-08-07 20:40:48 +0700

    update

--- SQL/Assignment 1/assignment_1.sql
+++ SQL/Assignment 1/assignment_1.sql
@@ -3,25 +3,25 @@ Create database testing_system_assignment_1;
 
 USE testing_system_assignment_1;
 DROP TABLE IF EXISTS department;
-create table department(
-			 department int ,
-             department_name varchar(50)
-             );
+CREATE TABLE department (
+    department INT,
+    department_name VARCHAR(50)
+);
 DROP TABLE IF EXISTS position;
-create table position (
-			 position_id int ,
-             position_name varchar(10)
-             );
+CREATE TABLE position (
+    position_id INT,
+    position_name VARCHAR(10)
+);
 DROP TABLE IF EXISTS account;
-create table account (
-			account_id int ,
-            email varchar(50),
-            user_name varchar(50),
-            full_name varchar(50),
-            department_id varchar(20),
-            Position_id int,
-            Create_date date
-            );
+CREATE TABLE account (
+    account_id INT,
+    email VARCHAR(50),
+    user_name VARCHAR(50),
+    full_name VARCHAR(50),
+    department_id VARCHAR(20),
+    Position_id INT,
+    Create_date DATE
+);
 
 
             
--- SQL/Assignment 1/extra_assignment_1.sql
+++ SQL/Assignment 1/extra_assignment_1.sql
@@ -10,7 +10,7 @@ CREATE TABLE trainee (
     ET_IQ INT unsigned CHECK (ET_IQ <= 20),
     ET_Gmath INT unsigned CHECK (ET_Gmath <= 20),
     ET_English INT unsigned CHECK (ET_English <= 50),
-    Tranining_class CHAR(6),
+    Tranining_class nvarchar(10),
     evaluation_notes TEXT
 );
 ALTER TABLE trainee 
@@ -31,5 +31,17 @@ CREATE TABLE datatypes2 (
     bỉrthday DATE,
     gender enum('0','1','2'),
     isdeletedflag enum('0','1')
-)
-            
+);
+INSERT INTO trainee (traineeid, fullname,birth_day,gender,ET_IQ,ET_Gmath,ET_English, Tranining_class,evaluation_notes,VTI_Account)
+VALUES ('1','TRAN VAN A','2003-07-03','male','15','12','30','toán','trung bình','tranvana@gmail.com'),
+		('2','TRAN VAN B','2002-08-02','male','20','9','10','lí','khá','tranvanb@gmail.com'),
+        ('3','TRAN VAN C','2004-09-02','male','11','12','30','hoá','khá','tranvanc@gmail.com'),
+        ('4','TRAN VAN D','2006-03-03','male','12','12','20','văn','giỏi','tranvand@gmail.com'),
+        ('5','TRAN VAN E','2009-09-03','male','11','5','30','văn','khá','tranvane@gmail.com'),
+        ('6','TRAN VAN F','2004-05-02','male','20','11','30','sinh','xuất sắc','tranvanf@gmail.com'),
+        ('7','TRAN VAN G','2006-09-12','male','15','12','30','anh','khá','tranvang@gmail.com'),
+        ('8','TRAN VAN H','2006-03-21','male','15','12','30','toán','trung bình','tranvanh@gmail.com'),
+        ('9','TRAN VAN I','2006-01-15','male','15','12','30','hoá','khá','tranvani@gmail.com');
+        
+           
+
