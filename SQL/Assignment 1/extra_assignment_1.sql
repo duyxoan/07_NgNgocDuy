d120b1e4f09282f497a550a378dc9b38bfacbd45
duyxoan <ngocduy060302@gmail.com>
2023-08-09 18:04:28 +0700

    update

--- SQL/Assignment 1/extra_assignment_1.sql
+++ SQL/Assignment 1/extra_assignment_1.sql
@@ -4,7 +4,7 @@ USE extra_system_assignment_1;
 DROP TABLE IF EXISTS Fresher_management;
 CREATE TABLE Fresher_management (
     traineeid INT ,
-    fullname NVARCHAR(50) NOT NULL,
+    fullname NVARCHAR(100) NOT NULL,
     birth_day DATE,
     gender ENUM('male', 'female', 'unknown'),
     ET_IQ INT unsigned CHECK (ET_IQ <= 20),
@@ -33,13 +33,13 @@ CREATE TABLE datatypes2 (
     isdeletedflag enum('0','1')
 );
 INSERT INTO Fresher_management (traineeid, fullname,birth_day,gender,ET_IQ,ET_Gmath,ET_English, Tranining_class,evaluation_notes,VTI_Account)
-VALUES ('1','TRAN VAN A','2003-07-03','male','15','12','30','toán','trung bình','tranvana@gmail.com'),
-		('2','TRAN VAN B','2002-08-02','male','20','9','10','lí','khá','tranvanb@gmail.com'),
-        ('3','TRAN THI C','2004-09-02','female','11','12','30','hoá','khá','tranthic@gmail.com'),
-        ('4','TRAN THI D','2006-03-03','female','12','12','20','văn','giỏi','tranthid@gmail.com'),
-        ('5','TRAN THI E','2009-09-03','female','11','5','30','văn','khá','tranthie@gmail.com'),
-        ('6','TRAN VAN F','2004-05-02','male','20','11','30','sinh','xuất sắc','tranvanf@gmail.com'),
-        ('7','TRAN VAN G','2006-09-12','male','15','12','30','anh','khá','tranvang@gmail.com'),
+VALUES ('1','Ngô Xuân Đông','2003-07-03','male','15','12','30','toán','trung bình','tranvana@gmail.com'),
+		('2','Trần Hữu Hưng','2002-08-02','male','20','9','10','lí','khá','tranvanb@gmail.com'),
+        ('3','Trịnh Kim Chi','2004-09-02','female','11','12','30','hoá','khá','tranthic@gmail.com'),
+        ('4','Lưu Thị Kim Dung','2006-03-03','female','12','12','20','văn','giỏi','tranthid@gmail.com'),
+        ('5','Nguyễn Hạ Quỳnh Thy','2009-09-03','female','11','5','30','văn','khá','tranthie@gmail.com'),
+        ('6','Nguyễn Ngọc Duy','2004-05-02','male','20','11','30','sinh','xuất sắc','tranvanf@gmail.com'),
+        ('7','Nguyễn Thị Giang','2006-09-12','female','15','12','30','anh','khá','tranvang@gmail.com'),
         ('8','TRAN VAN H','2006-03-21','male','15','12','30','toán','trung bình','tranvanh@gmail.com'),
         ('9','TRAN VAN I','2006-01-15','male','15','12','30','hoá','khá','tranvani@gmail.com');
        
@@ -47,3 +47,27 @@ VALUES ('1','TRAN VAN A','2003-07-03','male','15','12','30','toán','trung bình
     select * from Fresher_management;
     delete from Fresher_management;
     SET SQL_SAFE_UPDATES = 0;
+    -- ques2--
+   SELECT COUNT(traineeid ) as 'số học sinh đã qua bài thi đầu vào',month(birth_day) as 'tháng sinh' 
+   FROM Fresher_management
+   GROUP BY  month(birth_day);
+   -- ques3
+	select * from Fresher_management
+    where char_length(fullname) = (select max(char_length(fullname))
+    from Fresher_management);
+-- ques4-- 
+select * from Fresher_management
+where ET_IQ + ET_Gmath >=20 and
+	 ET_IQ >=8 and
+    ET_Gmath >=8 and 
+    ET_English >= 18 ;
+-- ques5
+delete  from Fresher_management
+where traineeid = 3;    
+-- ques6
+update Fresher_management
+set Tranining_class = 'lí'
+where traineeid= 5;
+    
+    
+    
